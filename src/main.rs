4260e08a1e5733e34d32d2b179cb7709a426c759
York Xiang <bombless@126.com>
2025-11-08 11:18:42 +0800

    Hang after calling `open_protocol_exclusive`

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 #![no_main]
 #![no_std]
 
-use core::panic::PanicInfo;
-
 use uefi::{prelude::*, println};
 
-use boot::protocols_per_handle;
+use uefi::proto::console::gop::GraphicsOutput;
 
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
-    println!("hello");
-    loop {}
 
-    Status::SUCCESS
+    println!("hello");
+
+    let gop_handle = boot::get_handle_for_protocol::<GraphicsOutput>().unwrap();
+
+    println!("got gop_handle {gop_handle:?}");
+    let mut gop = if let Ok(gop) = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle) {
+        gop
+    } else {
+        println!("Failed to open GraphicsOutput protocol");
+        loop {}
+    };
+
+    println!("gop");
+    // Create UefiDisplay
+    let mode = gop.current_mode_info();
+    println!("mode");
+
+    let frame_buffer = gop.frame_buffer();
+
+    println!("mode: {mode:?}, frame_buffer: {frame_buffer:?}");
+    loop {}
 }
